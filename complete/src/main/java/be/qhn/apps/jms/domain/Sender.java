377659fba83edbe626939d37a347cad166b28471
Raymond Loman <raymond@carpago.nl>
2018-02-14 22:20:22 +0100

    Some experimental and running cron to have no loop but a cron expression

--- complete/src/main/java/be/qhn/apps/jms/domain/Sender.java
+++ complete/src/main/java/be/qhn/apps/jms/domain/Sender.java
@@ -1,9 +1,10 @@
 package be.qhn.apps.jms.domain;
 
-import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,27 +13,11 @@ public class Sender {
    @Autowired
    private JmsTemplate jmsTemplate;
 
-   @PostConstruct
-   public void init() {
-      
-      for(;;) {
-         try {
-            Thread.sleep(5000);
-            send();
-         }
-         catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         }
-      }
-     
-      
-   }
-   
+   @Scheduled(cron="*/10 * * * * *")
    public void send() {
       // Send a message with a POJO - the template reuse the message converter
       System.out.println("Sending an email message.");
-      jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+      jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello at timestamp "+LocalDateTime.now()));
       
       System.out.println("Sending a feature request to support");
       jmsTemplate.convertAndSend("featureIn", new Feature(1, "Raadplegen VA", "Raadplegen VA test als 1 dan return 333.33"));
