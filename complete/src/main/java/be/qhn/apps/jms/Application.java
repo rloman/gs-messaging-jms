8fbb627fe58f5690205cf40b5b3d0ebe74e827ac
Raymond Loman <raymond@carpago.nl>
2018-02-14 21:17:24 +0100

    cleanup

--- complete/src/main/java/be/qhn/apps/jms/Application.java
+++ complete/src/main/java/be/qhn/apps/jms/Application.java
@@ -1,31 +1,16 @@
 
 package be.qhn.apps.jms;
 
-import javax.jms.ConnectionFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
 @EnableJms
 public class Application {
    
     public static void main(String[] args) {
-        // Launch the application
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-//        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+          SpringApplication.run(Application.class, args);
     }
 
 }
